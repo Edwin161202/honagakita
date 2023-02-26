fc43df11474dddbdcbcb7a7f9da948f7a8858a54
Muhamad Riyanto <muhamad.riyanto185@gmail.com>
2023-02-26 08:21:38 -0500

    belum selesai

---
+++ script.js
@@ -0,0 +1,6 @@
+const namePerson = ['sinyo', 'gendut', 'salman', 'agus', 'menyok', 'pak andi', 'bu icha', 'yoga', 'rosi','reza','jaelani','magang1', 'magang2', 'magang3', 'magang4', 'magang5', 'jodha', 'mak tiri', 'mbak dewi', 'kukuh', 'bu elok', 'jun', 'riyan', 'aris', 'bu lilik','dewor', 'linda', 'putri', 'pipit', 'salma', 'tini','mbak inces', ];
+
+const allName = namePerson.forEach((value, index) =>  {
+    console.log( `Nomer ${index +1} adalah ${value}`);
+});
+// console.log(allName);
